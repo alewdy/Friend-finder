60958668d699d7657084e8b0d24f00ebd3200caf
Alewdy <alewdys@gmail.com>
2018-08-12 22:52:29 -0400

    app

---
+++ app/data/friend.js
@@ -0,0 +1,23 @@
+
+
+
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://cdn.pixabay.com/photo/2018/07/20/22/43/adler-3551609_1280.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+            ]
+    }
+];
+
+module.exports = friends;
