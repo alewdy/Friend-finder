08272700dddc91f01375edf008b1be3cc1ace705
alewdy <you@example.com>
2018-08-16 17:42:29 -0400

    friend

--- app/data/friend.js
+++ app/data/friend.js
@@ -5,7 +5,7 @@
 var friends = [
     {
         name: "Ahmed",
-        photo: "https://images.unsplash.com/photo-1534361505346-d0fadfd493b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e94b853ec1f081895f21345c3a55ef01&auto=format&fit=crop&w=334&q=80",
+        photo: "https://images.unsplash.com/photo-1534361505346-d0fadfd493b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e94b853ec1f081895f21345c3a55ef01&auto=format&fit=crop&w=334&q=80`",
         scores:[
             5,
             1,
